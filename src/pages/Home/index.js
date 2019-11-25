8f11f020465c90cdde3e8a11be102126f4591b72
Maicon <Maicon@MacBook-Air-de-Maicon.local>
2019-11-24 21:20:42 -0300

    add saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,9 +22,9 @@ class  Home extends Component {
         this.setState({products:data});
     };
 
-    handleAddProduct = product =>{
-        const { addToCart } = this.props;
-        addToCart(product);
+    handleAddProduct = id =>{
+        const { addToCartRequest } = this.props;
+        addToCartRequest(id);
     };
 
     render() {
@@ -42,7 +42,7 @@ class  Home extends Component {
                     />
                     <strong>{product.title}</strong>
                     <span>{ product.priceFormatted }</span>
-                    <button type="button" onClick={ () => this.handleAddProduct(product)}>
+                    <button type="button" onClick={ () => this.handleAddProduct(product.id)}>
                         <div>
                             <MdAddShoppingCart size={16} color="#fff"/>{''}
                             {amount[product.id] || 0}
