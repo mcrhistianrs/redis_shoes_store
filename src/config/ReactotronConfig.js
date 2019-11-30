fa007c1d59fc49ce085ff87df948e1a9d6fbbb8a
Maicon <Maicon@MacBook-Air-de-Maicon.local>
2019-11-30 15:24:58 -0300

    reactotron+saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,8 +1,9 @@
 import Reacttotron          from 'reactotron-react-js';
 import { reactotronRedux }  from 'reactotron-redux';
+import reactotronSaga       from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development'){
-    const tron = Reacttotron.configure().use(reactotronRedux()).connect();
+    const tron = Reacttotron.configure().use(reactotronRedux()).use(reactotronSaga()).connect();
     tron.clear();
     console.tron = tron;
 }
